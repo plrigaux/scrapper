a15ec4e0c48d8a0fa74a16936033a89d2ff3bde5
Pier <491172+plrigaux@users.noreply.github.com>
2022-02-21 19:08:34 -0500

    old file

---
+++ testp.py
@@ -0,0 +1,11 @@
+print("dir", "D:/media/xtreamdownloader/Liya%20Silver,%20Talia%20Mint%20-%20Table%20Tennis%20Treats")
+
+print("dir", "D:/media/xtreamdownloader")
+
+print("dir", "D:/media/xtreamdownloader/Liya")
+
+print("gg", r"D:\media\xtreamdownloader\Liya Silver, Talia Mint - Table Tennis Treats")
+
+print("gg", "\"D:/media/xtreamdownloader/Liya Silver, Talia Mint - Table Tennis Treats\"")
+
+print("gg", "D:/media/xtreamdownloader/Liya\ Silver,\ Talia Mint\ -\ Table\ Tennis\ Treats")
